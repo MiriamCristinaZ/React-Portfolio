4b316c9809db546ec1cd917650c931be0ead7b64
Miriam Cristina Zarzu <145344909+MiriamCristinaZ@users.noreply.github.com>
2024-01-29 22:00:57 +0000

    chnaged the project outline

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Project({ project }) {
+  const imageStyles = {
+    maxWidth: "100%",
+    maxHeight: "300px", // Adjust the maximum height as needed
+  };
+
   return (
-    <div>
-      <h3>{project.title}</h3>
-      <a href={project.deployedLink}>Deployed Project</a>
-      <a href={project.githubLink}>GitHub Repo</a>
-      <img src={project.image} alt={`Screenshot of ${project.title}`} />
+    <div className="col-lg-4 col-md-6 mb-4">
+      {" "}
+      {/* Use col-lg-4 for large screens and col-md-6 for medium screens */}
+      <div className="card">
+        <img
+          src={project.image}
+          alt={`Screenshot of ${project.title}`}
+          style={imageStyles}
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h3 className="card-title">{project.title}</h3>
+          <a href={project.deployedLink} className="btn btn-primary mr-2">
+            Deployed Project
+          </a>
+          <a href={project.githubLink} className="btn btn-secondary">
+            GitHub Repo
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
@@ -20,4 +40,5 @@ Project.propTypes = {
     image: PropTypes.string.isRequired,
   }).isRequired,
 };
+
 export default Project;
